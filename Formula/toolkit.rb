d64464c749f7c9c2806f1a2f344cf9188168af2f
jim <jim@wangxutech.com>
2024-08-08 16:18:22 +0800

    toolkit

---
+++ Formula/toolkit.rb
@@ -0,0 +1,17 @@
+class Toolkit < Formula
+  desc "自动化办公工具集，助力高效办公！"
+  homepage "https://blog.ip21.cn/"
+  url "https://cdn.ip21.cn/soft/homebrew/tools/tools.tar.gz"
+  version "1.0.0"
+  sha256 "2284172434443448c46de36674a548706d47daf8aa46761ce98be7809dc775c1"
+
+  def install
+    # 假设你的二进制文件是可执行的
+    bin.install "toolkit"
+  end
+
+  test do
+    # 测试你的软件是否正确安装
+    system "#{bin}/toolkit", "--version"
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# homebrew-toolkit
-homebrew-toolkit 工具箱
+# homebrew
+
+## toolkit 工具箱
+
+### macOS安装方式
+
+* 添加tap源信息
+```shell
+brew tap goldenwind/homebrew
+```
+
+* 安装工具箱 应用程序： 
+```shell
+brew install goldenwind/homebrew/toolkit
+```
+
