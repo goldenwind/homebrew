7b116883af5cda82e177e8a0549e434acd1902e5
jim <jim@wangxutech.com>
2024-08-09 15:39:27 +0800

    Windkit

--- Formula/windkit.rb
+++ Formula/windkit.rb
@@ -1,9 +1,9 @@
 class Windkit < Formula
   desc "自动化办公工具集，助力高效办公！"
   homepage "https://blog.ip21.cn/"
-  url "https://github.com/goldenwind/homebrew/releases/download/v1.0.0/windkit.tar.gz"
+  url "https://github.com/goldenwind/homebrew-brew/releases/download/v1.0.0/windkit.tar.gz"
   version "1.0.0"
-  sha256 "664dfe3db1c3671d6118607e0d6af655bdec4dac2828ce89e36df135cf0441b2"
+  sha256 "b8a67be0cd7afab8a26a2b6d0fe629b01630afa712698e807e880b98b91161f0"
 
   def install
     # 假设你的二进制文件是可执行的
